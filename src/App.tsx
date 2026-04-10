4e42440294fe422f0ce2f1b35d718618537b0258
José Sebastián García Escobar <Sebastian@MacBook-Air-de-Mariana-67.local>
2026-04-10 12:00:52 -0600

    Corregido salto de linea literal para evitar error en compilacion de Vercel

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,8 @@ import BudgetingSection from './components/BudgetingSection';
 import Testimonials from './components/Testimonials';
 import Blog from './components/Blog';
 import CTA from './components/CTA';
-import Contact from "./components/Contact";\nimport Footer from './components/Footer';
+import Contact from "./components/Contact";
+import Footer from './components/Footer';
 
 export default function App() {
   return (
@@ -21,7 +22,8 @@ export default function App() {
         <Blog />
         <CTA />
       </main>
-      <Contact />\n      <Footer />
+      <Contact />
+      <Footer />
     </div>
   );
 }
